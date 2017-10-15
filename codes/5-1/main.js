e36753b82476413acd007a6800a72cb8487c3e47
wuhaolin <wuhaolin@meituan.com>
2017-10-15 12:49:38 +0800

    fix: 工作原理概括

--- codes/5-1/main.js
+++ codes/5-1/main.js
@@ -1,4 +1,2 @@
-// 通过 CommonJS 规范导入 show 函数
-const show = require('./show.js');
-// 执行 show 函数
+import show from './show';
 show('Webpack');
--- codes/5-1/show.js
+++ codes/5-1/show.js
@@ -1,7 +1,3 @@
-// 操作 DOM 元素，把 content 显示到网页上
-function show(content) {
-  window.document.getElementById('app').innerText = 'Hello,' + content;
+export default function show(content) {
+  window.document.getElementById('app').innerText = 'Hello2,' + content;
 }
-
-// 通过 CommonJS 规范导出 show 函数
-module.exports = show;
